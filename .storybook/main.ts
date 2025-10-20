44ffc3edc7e1f0dcfd9d8d07d0357606c82469f6
viba98 <32447352+viba98@users.noreply.github.com>
2025-10-20 13:18:56 -0700

    make storybook work

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,17 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import { join, dirname, resolve } from "path";
+import { fileURLToPath } from 'url';
 
-import { join, dirname, resolve } from "path"
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
 * This function is used to resolve the absolute path of a package.
 * It is needed in projects that use Yarn PnP or are set up within a monorepo.
 */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+function getAbsolutePath(value: string): string {
+  return resolve(__dirname, '../node_modules', value);
 }
+
 const config: StorybookConfig = {
   "stories": [
     "../apps/www/registry/elevenlabs-ui/ui/**/*.mdx",
